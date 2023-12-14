ea9d17ec913de7fd5cf6cafc8f497673684fad20
mgarbacz <michal@garbacz.io>
2023-12-14 18:14:18 -0500

    Export default function and return a promise
    instead of just logging to console

--- day1/solution.js
+++ day1/solution.js
@@ -1,25 +1,35 @@
 import readline from 'node:readline';
 import fs from 'node:fs';
 
-const fileStream = fs.createReadStream(new URL('./input.txt', import.meta.url));
-let calibrationValueSum = 0;
+const day1 = async function() {
+  const filePath = new URL('./input.txt', import.meta.url);
+  const fileStream = fs.createReadStream(filePath);
+  let calibrationValueSum = 0;
 
-const rl = readline.createInterface({
-  input: fileStream,
-  crlfDelay: Infinity,
-});
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
-rl.on('line', (line) => {
-  const digits = line.match(/\d/g);
+  rl.on('line', (line) => {
+    const digits = line.match(/\d/g);
 
-  if (digits && digits.length > 0) {
-    const firstDigit = digits[0];
-    const lastDigit = digits[digits.length - 1];
-    
-    calibrationValueSum += parseInt(firstDigit + lastDigit);
-  }
-});
+    if (digits && digits.length > 0) {
+      const firstDigit = digits[0];
+      const lastDigit = digits[digits.length - 1];
+      
+      calibrationValueSum += parseInt(firstDigit + lastDigit);
+    }
+  });
 
-rl.on('close', () => {
-  console.log(calibrationValueSum);
-});
+  return new Promise((resolve, reject) => {
+    rl.on('close', () => {
+      resolve(calibrationValueSum);
+    });
+
+    rl.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+export default day1;
