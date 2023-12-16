12377a47eaeb135f9f52aed12157d3b839374a37
mgarbacz <michal@garbacz.io>
2023-12-16 00:43:32 -0500

    Set up regex and indexes to check

--- day3/solution.js
+++ day3/solution.js
@@ -72,8 +72,28 @@ const containsDigits = function(str) {
   return digitRegex.exec(str) !== null;
 };
 
-export const parseGearRatio = function(line) {
-  return 0;
+export const parseGearRatio = function(nextLine, line, prevLine) {
+  const asteriskRegex = /\*/g;
+  let lineTotal = 0;
+
+  let match;
+  while ((match = asteriskRegex.exec(line)) !== null) {
+    // Digits can be anywhere around the asterisk, including diagonals, so we
+    // will be checking from the matching index - 1 (before) all the way to the
+    // matching index + match length (after) within the bounds of the string
+    const before = (match.index - 1 > 0) ? match.index - 1 : 0;
+    const after = (match.index + match[0].length < line.length) ?
+      match.index + match[0].length : line.length;
+    const beforeCheck = line[before];
+    const afterCheck = line[after];
+    const prevCheck = prevLine.substring(before, after + 1);
+    const nextCheck = nextLine.substring(before, after + 1);
+
+    // TODO: check digits around the asterisk, recursively reconstruct the whole
+    // number there, and then multiple them together to add to the lineTotal
+  }
+
+  return lineTotal;
 };
 
 export default day3;
